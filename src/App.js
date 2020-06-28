14fe97655d5e35d53c5ea0c7b0cbd42a6fe6bdc7
Jesus Silva <jjesussh31@gmail.com>
2020-06-28 13:16:30 -0500

    leyendo datos de usuario | validando formulario |  escribiendo funcion  para api | obteniendo datos de api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import React, {Fragment}from 'react';
+import React, { Fragment, useState, useEffect, } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
 function App() {
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [consultar, guardarConsultar] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarApi = async () => {
+    
+    if (consultar) {
+      const appId = "";
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}, ${pais}&appid=${appId}`;
+
+      const response = await fetch(url);
+      const resultado = response.json();
+
+      console.log(resultado);
+      
+       
+
+      }
+    }
+
+    consultarApi();
+  }, [consultar])
+
   return (
     <Fragment>
-        <Header 
-          titulo = "ClIMA REACT APP"
-        />
-        <div className = "contenedor-form">
-          <div className = "container">
-            <div className = "row">
-              <div className = "col m6 s12">
-                <Formulario />
-              </div>
-              <div className = "col m6 s12">
-                2
-              </div>
+      <Header
+        titulo="CLIMA REACT APP"
+      />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                guardarBusqueda={guardarBusqueda}
+                guardarConsultar={guardarConsultar}
+              />
             </div>
+            <div className="col m6 s12">
+              2
+              </div>
           </div>
         </div>
+      </div>
     </Fragment>
-   
+
   );
 }
 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,30 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Formulario = () => {
-    return (  
-        <form>
-            <div className = "input-field col s12">
-                <input 
-                    type = "text"
-                    name = "ciudad"
-                    id = "ciudad"
+const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
+   
+
+    const [error, guardarError] = useState(false);
+
+    //extraer ciudad y pais
+    const { ciudad, pais } = busqueda;
+
+    //funcion que coloca los elementos en el state
+    const handleChange = e => {
+        //actualizar state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //validar
+        if (ciudad.trim() === '' || pais.trim() === '') {
+            guardarError(true);
+            return
+        }
+        guardarError(false);
+
+
+        //enviar al componente principal
+        guardarConsultar(true)
+    }
+
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <p className="red darken-4 error">Los campos no deben de estar vacios</p> : null}
+            <div className="input-field col s12">
+                <input
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
                 />
-                <label htmlFor = "ciudad">Ciudad</label>
+                <label htmlFor="ciudad">Ciudad</label>
             </div>
 
-            <div className = "input-field col s12">
+            <div className="input-field col s12">
                 <select
-                    name = "pais"
-                    id = "pais"
+                    name="pais"
+                    id="pais"
+                    value={pais}
+                    onChange={handleChange}
                 >
                     <option>-- Seleccione un pais--</option>
-
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
-                <label htmlFor = "pais">Pais: </label>
+                <label htmlFor="pais">Pais: </label>
+
+            </div>
+
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    value="Buscar clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
 
             </div>
         </form>
     );
 }
- 
+
 export default Formulario;
